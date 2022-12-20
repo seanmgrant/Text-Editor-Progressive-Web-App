e214196a82fac8cdbe91431246ad739965014309
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-12-20 12:53:46 -0700

    Webpack updated chnage names

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// : Add and configure workbox plugins for a service worker and manifest file.  
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -18,11 +18,41 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'Progressive Web App',
+      }),
+      new WebpackPwaManifest({
+        name: 'PWA App',
+        short_name: 'PWA',
+        description: 'My PWA App!',
+        background_color: '#01579b',
+        theme_color: '#ffffff',
+        star_url: '/',
+        pulicpath: '/',
+        fingerprints: false,
+        inject: false,
+      }),
       
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              Plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        }
         
       ],
     },
